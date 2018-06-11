35d33c80d9b9d77bdb277ae4dbf0b45a26948818
Pawel Chmielak <chmielok@gmail.com>
2018-06-11 19:34:08 +0200

    Data downloading without automatic moving to directories

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+kaggle competitions download -c forest-cover-type-prediction
+kaggle datasets download -d uciml/human-activity-recognition-with-smartphones
+
+# Move the data to directories forest/ and human_activity/
