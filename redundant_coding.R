4f12dc9e41ca24fc1676b551b84c796058735f5e
Dawid Glab <glab.dawid@gmail.com>
2018-06-11 19:29:08 +0200

    Comments added to redundant_coding.R

--- redundant_coding.R
+++ redundant_coding.R
@@ -1,3 +1,6 @@
+# Returns matrix of bit_codes (shrinked if necessary)
+# Requires:
+#   nClasses - number of unique classes
 code_matrix <- function(nClasses) {
   n <- code_length(nClasses)
   
@@ -7,23 +10,36 @@ code_matrix <- function(nClasses) {
   }
   mat = matrix(vct, nrow = nClasses, ncol = n)
   
+  # shrink by random if necessary
   if(nClasses > 6) {
     mat = choose_random_columns(mat, code_length(6))
   }
 
-    return(mat[do.call(order, as.data.frame(mat)),])
+  return(mat[do.call(order, as.data.frame(mat)),])
 }
 
+# Returns list of bit_codes.
+# Used by redundancy_predictor.
+# Requires:
+#   nClasses - number of unique classes
 bit_codes = function(nClasses) {
   mtrx = code_matrix(nClasses)
   return(apply(mtrx, 1, paste, collapse=''))
 }
 
+# Shrinks matrix by returning random columns.
+# Requires:
+#   mat - matrix to be shrinked
+#   size - desired number of columns
 choose_random_columns <- function(mat, size) {
   j <- sample(seq_len(ncol(mat)), size=size)
   return(mat[, j, drop=FALSE])
 }
 
+# Converts decimal number to bit vector
+# Requires:
+#   number - number to be converted
+#   noBits - desired length of vector
 number2binary = function(number, noBits) {
   binary_vector = rev(as.numeric(intToBits(number)))
   if(missing(noBits)) {
@@ -33,6 +49,9 @@ number2binary = function(number, noBits) {
   }
 }
 
+# Returns length of bit code used in redundancy_predictor
+# Requires:
+#   nClasses - number of unique classes
 code_length = function(nClasses) {
   if(nClasses > 6) {
     return(code_length(6))
@@ -40,6 +59,10 @@ code_length = function(nClasses) {
   return(2 ^ (nClasses - 1) - 1)
 }
 
+# Returns corrected bit codes given by prediction
+# Requires:
+#   labels - labels to be corrected
+#   classes - all proper bit codes
 correct_labels <- function(labels, classes) {
   classes_matrix <- unlist(strsplit(classes, ''))
   classes_matrix <- matrix(as.numeric(classes_matrix), ncol=length(classes))
@@ -48,6 +71,10 @@ correct_labels <- function(labels, classes) {
   return(classes[indices])
 }
 
+# Find closest bit code. Hamming distance is used to calculate difference.
+# Requires:
+#   label - bit code to be corrected
+#   code_matrix - matrix of all bit codes
 find_closest <- function(label, code_matrix) {
   neighbors <- apply(code_matrix, 1, hamming.distance, unlist(label, use.names=FALSE))
   return(which.min(neighbors))
